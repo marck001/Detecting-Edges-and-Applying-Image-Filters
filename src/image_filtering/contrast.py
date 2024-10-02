46c040c6477dc2eaa7ec68539e450bf767c7a6c9
Mac <margeo124@gmail.com>
2024-10-01 22:53:33 -0500

    First commit

---
+++ src/image_filtering/contrast.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def apply_contrast(image):
+  img = cv2.imread(image, 0)
+  
+  histeq = cv2.equalizeHist(img)
+  cv2.imshow('Input', img)
+  cv2.imshow('Histogram equalized', histeq)
+  cv2.waitKey(0)
